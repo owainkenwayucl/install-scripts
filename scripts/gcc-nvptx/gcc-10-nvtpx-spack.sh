a9ecf42256150e0b35037da2933286dfe412589f
Owain Kenway <o.kenway@ucl.ac.uk>
2021-02-15 12:57:35 +0000

    Require up to date python.

--- scripts/gcc-nvptx/gcc-10-nvtpx-spack.sh
+++ scripts/gcc-nvptx/gcc-10-nvtpx-spack.sh
@@ -13,6 +13,7 @@ ln -fs /shared/ucl/apps/build_scripts/includes .
 for i in ${includes_dir:=$(dirname $0 2>/dev/null)/includes}/{module_maker,require}_inc.sh; do . $i; done
 
 require gcc-libs/4.9.2
+require python/3.9.1
 
 NAME=${NAME:-gcc-nv-10}
 VERSION=${VERSION:-spack}
