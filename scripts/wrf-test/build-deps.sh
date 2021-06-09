471bc410541c98b18e577d736a18b686b6f32187
Owain Kenway <o.kenway@ucl.ac.uk>
2021-06-09 17:02:53 +0100

    Script to build dependencies.

---
+++ scripts/wrf-test/build-deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -Rf ~/Source/wrf/deps
+mkdir -p ~/Source/wrf/deps
+cd ~/Source/wrf/deps
+
+rm -Rf ~/.spack
+git clone https://github.com/spack/spack
+
+module load python/3.8.6
+
+cd spack
+module use --append $(pwd)/share/spack/modules/linux-rhel7-cascadelake
+bin/spack install gcc@11.1.0
+
+bin/spack compiler find
+
+bin/spack install wrf@4.2%gcc@11.1.0
+
