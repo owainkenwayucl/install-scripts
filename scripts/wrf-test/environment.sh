f26056731c0b6be3ae7800dce9b6f28fdf89ec59
Owain Kenway <o.kenway@ucl.ac.uk>
2021-06-11 15:01:59 +0100

    Added NetCDF Fortran + C binding paths.

--- scripts/wrf-test/environment.sh
+++ scripts/wrf-test/environment.sh
@@ -42,7 +42,11 @@ module use --append ~/Source/wrf/deps/spack/share/spack/modules/$spackarch
 module load $modopenmpi $modnetcdfp $modnetcdfc $modnetcdff $modjasp $modpng $modz $modperl $modhdf $modm4 $modlt
 
 cd ~/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0
-ncdir=$(ls | grep netcdf-fortran-*)
+ncdir=$(ls | grep netcdf-fortran-)
+nccdir=$(ls | grep netcdf-c-)
 export NETCDF="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$ncdir"
 export NETCDF_classic=1
+export NETCDFF=$NETCDF
+export NETCDFC="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$nccdir"
+
 cd $oldpwd
