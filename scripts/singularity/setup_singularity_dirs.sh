da1200eeb9cd3c393d96a61d0695ac6bd4348bca
Owain Kenway <o.kenway@ucl.ac.uk>
2021-08-04 18:02:47 +0100

    Script to create singularity working directories

---
+++ scripts/singularity/setup_singularity_dirs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Create "." dir on Scratch space
+mkdir -p ${HOME}/Scratch/singularity
+
+mkdir -p ${HOME}/Scratch/singularity/tmp
+mkdir -p ${HOME}/Scratch/singularity/localcache
+mkdir -p ${HOME}/Scratch/singularity/pull
+
