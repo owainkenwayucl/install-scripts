e8b9104efbc3d70290c5e743297e68a761cdce4f
Owain Kenway <o.kenway@ucl.ac.uk>
2022-04-12 10:46:17 +0100

    HPL submission.

---
+++ scripts/hpl/input/submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+#$ -l h_rt=24:00:00
+
+#$ -l mem=5300M
+
+#$ -wd /home/uccaoke/Scratch
+
+#$ -pe mpi 80
+
+#$ -l threads=1
+
+#$ -P Unavailable
+##$ -A Test_allocation
+
+#$ -t 1-10000
+
+#$ -N Trogdor
+module load compilers/intel/2018/update3
+module load mpi/intel/2018/update3/intel
+
+INPUTDIR=$(pwd)
+INPUTFILE=${INPUTFILE:-kathleen-singlenode.dat}
+
+mkdir -p Trogdor-$[JOB_ID]-${SGE_TASK_ID}
+cd Trogdor-$[JOB_ID]-${SGE_TASK_ID}
+
+ln -s ${INPUTDIR}/${INPUTFILE} ./HPL.dat
+
+export OMP_NUM_THREADS=2
+
+while true; do
+  gerun xhpl
+done
