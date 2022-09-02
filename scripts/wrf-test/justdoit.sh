4a87bf1831d4baae416075d746a30435c50facf8
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-02 13:06:13 +0100

    Full build

--- scripts/wrf-test/justdoit.sh
+++ scripts/wrf-test/justdoit.sh
@@ -5,8 +5,13 @@
 set -e
 
 mkdir -p ~/Source/wrf/deps
+mkdir -p ~/Source/wps
 
 ./build-deps.sh
 
-./wrf-4.3.sh
+#./wrf-4.3.sh
 ./wrf-4.4.1.sh
+
+ln -s $HOME/Source/wrf/builds/4.4.1 $HOME/Source/wps/builds/WRF
+
+./wps-4.4.sh
