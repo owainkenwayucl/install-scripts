18a10cfc07d8ef59419fcee67feb9d0ac91e5189
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-05 12:13:38 +0100

    Libjpeg

--- scripts/wrf-test/environment.sh
+++ scripts/wrf-test/environment.sh
@@ -24,13 +24,15 @@ modperl=$(ls | grep perl)
 modhdf=$(ls | grep hdf5)
 modm4=$(ls | grep m4)
 modlt=$(ls | grep libtool)
+modjpeg=$(ls | grep libjpeg)
 
 echo "OpenMPI module:         $modopenmpi"
 echo "NetCDF parallel module: $modnetcdfp"
 echo "NetCDF C module:        $modnetcdfc"
 echo "NetCDF Fortran module:  $modnetcdff"
 echo "Jasper module:          $modjasp"
-echo "PNG modue:              $modpng"
+echo "PNG module:             $modpng"
+echo "JPED module             $modjpeg"
 echo "Zlib module:            $modz"
 echo "Perl module:            $modperl"
 echo "HDF5 module:            $modhdf"
@@ -39,12 +41,13 @@ echo "Libtool module:         $modlt"
 
 module use --append ~/Source/wrf/deps/spack/share/spack/modules/$spackarch
 
-module load $modopenmpi $modnetcdfp $modnetcdfc $modnetcdff $modjasp $modpng $modz $modperl $modhdf $modm4 $modlt
+module load $modopenmpi $modnetcdfp $modnetcdfc $modnetcdff $modjasp $modpng $modz $modperl $modhdf $modm4 $modlt $modjpeg
 
 cd ~/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0
 ncdir=$(ls | grep netcdf-fortran-)
 nccdir=$(ls | grep netcdf-c-)
 jaspdir=$(ls | grep jasper-)
+jpegdir=$(ls | grep libjpeg-)
 
 echo "BRAVELY MERGING NETCDF!!!"
 rsync -av "$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$nccdir/" "$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$ncdir/"
@@ -57,6 +60,6 @@ export NETCDFF=$NETCDF
 export NETCDFC="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$nccdir"
 export JASPERLIB="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$jaspdir/lib64"
 export JASPERINC="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$jaspdir/include"
-export LD_LIBRARY_PATH="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$ncdir/lib:$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$nccdir/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$jpegdir/lib64:$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$ncdir/lib:$HOME/Source/wrf/deps/spack/opt/spack/$spackarch/gcc-9.2.0/$nccdir/lib:$LD_LIBRARY_PATH"
 
 cd $oldpwd
