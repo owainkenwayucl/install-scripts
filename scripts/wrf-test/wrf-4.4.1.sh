57e1a86f0234f03dbc678270b97fe9259a75763f
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-07 14:23:33 +0100

    Make changes discussed at yesterday's meeting.
    1. Build from archive - for "reasons" the archive contains extra functionality that the repo does not.
    
    2. Enable patching of Oscar's rank patch.

--- scripts/wrf-test/wrf-4.4.1.sh
+++ scripts/wrf-test/wrf-4.4.1.sh
@@ -12,15 +12,38 @@ NAME=${NAME:-wrf}
 VERSION=${VERSION:-4.4.1}
 #INSTALL_PREFIX=${INSTALL_PREFIX:-/shared/ucl/apps/$NAME/$VERSION/$COMPILER_TAG}
 INSTALL_PREFIX=${INSTALL_PREFIX:-${HOME}/Source/wrf/builds}
-SRC_ARCHIVE=${SRC_ARCHIVE:-https://github.com/wrf-model/WRF/}
+GIT_REPO=${GIT_REPO:-https://github.com/wrf-model/WRF/}
+SOURCE_ARCHIVE=${SOURCE_ARCHIVE:-https://github.com/wrf-model/WRF/releases/download/v${VERSION}/v${VERSION}.tar.gz}
+GIT_BUILD=${GIT_BUILD:-no}
+UNLOCK_RANKS=${UNLOCK_RANKS:-no}
+
+orig_dir=$(pwd)
 
 rm -Rf $INSTALL_PREFIX/$VERSION
 mkdir -p $INSTALL_PREFIX
-
 cd $INSTALL_PREFIX
-git clone $SRC_ARCHIVE $VERSION
-cd $VERSION
-git checkout v$VERSION
+
+if [ "${GIT_BUILD^^}" == "YES" ]; then
+   git clone $SRC_ARCHIVE $VERSION
+   cd $VERSION
+   git checkout v$VERSION
+
+else
+   wget $SOURCE_ARCHIVE
+   # Note we are https:ing this from the same location as the git repo so no
+   # need to checksum.
+   tar xvf v${VERSION}.tar.gz
+   mv WRFV${VERSION} ${VERSION}
+   cd $VERSION
+fi
+
+if [ "${UNLOCK_RANKS^^}" == "YES" ]; then
+   # Apply Oscar's patch to unlock the rank limits.
+   cd share
+   cp ${orig_dir}/patches/disable_rank_check.patch .
+   patch < disable_rank_check.patch
+   cd ..
+fi
 
 ./configure
 ./compile -j 4 em_real
