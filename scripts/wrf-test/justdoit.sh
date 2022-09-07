314203a1e049110c03e961dd692067c4557d459a
Owain Kenway <o.kenway@ucl.ac.uk>
2022-09-07 14:25:43 +0100

    Add wrf flags to justdoit.sh

--- scripts/wrf-test/justdoit.sh
+++ scripts/wrf-test/justdoit.sh
@@ -4,13 +4,16 @@
 
 set -e
 
+GIT_BUILD=${GIT_BUILD:-no}
+UNLOCK_RANKS=${UNLOCK_RANKS:-no}
+
 mkdir -p ~/Source/wrf/deps
 mkdir -p ~/Source/wps/builds
 
 ./build-deps.sh
 
 #./wrf-4.3.sh
-./wrf-4.4.1.sh
+GIT_BUILD=${GIT_BUILD} UNLOCK_RANKS=${UNLOCK_RANKS} ./wrf-4.4.1.sh
 
 ln -s $HOME/Source/wrf/builds/4.4.1 $HOME/Source/wps/builds/WRF
 
