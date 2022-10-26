022766ab2d9cfaeec7e52b15420e46005691630f
Owain Kenway <o.kenway@ucl.ac.uk>
2022-10-26 13:53:21 +0100

    Spack test environment

---
+++ scripts/spack-test/install_spack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+
+
+set -e
+module purge
+module load gcc-libs/4.9.2
+module load compilers/gnu/4.9.2
+
+cd ~/Applications
+rm -Rf ~/Applications/spack
+rm -Rf ~/.spack
+git clone https://github.com/spack/spack
+
+cd spack
+bin/spack install python@3.11.0
+
+arch=`ls share/spack/modules/linux-rhel7*`
+
+ln -s `pwd`/$arch share/spack/modules/ucl-linux
