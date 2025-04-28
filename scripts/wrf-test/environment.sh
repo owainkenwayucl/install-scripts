d00b645bde69752182f89b0a5457ea881b63bbb9
Owain Kenway <o.kenway@ucl.ac.uk>
2025-04-28 10:10:12 +0100

    Bump python version

--- scripts/wrf-test/environment.sh
+++ scripts/wrf-test/environment.sh
@@ -4,7 +4,7 @@ module purge
 module load beta-modules
 module load gcc-libs/9.2.0
 module load compilers/gnu/9.2.0
-module load python/3.8.6
+module load python/3.11.4
 module load git
 module load cmake
 
